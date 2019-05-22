b994ef53d8fa96a40ba2e22723801e9a127e41cf
YA <YA-androidapp@users.noreply.github.com>
2019-05-23 08:47:54 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2019 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
+
+import datetime
+from glob import glob
 import configparser
+import os
 import requests
 import json
+
+
+scrpath = os.path.abspath(os.path.dirname(__file__))
+os.chdir(scrpath)
+
+# このスクリプトと同じディレクトリにdatasetフォルダを作成、
+# そのサブディレクトリに訓練データと検証データからなるデータセットを格納
+root_test_dirname = 'test'
+
+# テスト結果を出力するテキストファイル名
+nowstr = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+result_filename = 'result-test-'+nowstr+'.txt'
+
+# 正答率評価のための変数
+count_items_all = 0
+count_items_correct = 0
 
 # 設定ファイル読み込み
 inifile = configparser.ConfigParser()
@@ -25,12 +46,45 @@ def classify(path):
             analysis = response.json()
             name, pred = analysis['predictions'][0]['tagName'], analysis['predictions'][0]['probability']
             print(name, pred)
+            return name
     except Exception as e:
         print('[Err] {0}'.format(e))
+    return ''
 
 
 def main():
-    classify('test.png')
+    global count_items_all
+    global count_items_correct
+
+    # テスト用画像取得
+    subdirs = glob(os.path.join(
+        scrpath, root_test_dirname, '**'))
+    for subdir in subdirs:
+        if os.path.isdir(subdir):
+            print('sub directory: {}'.format(subdir), datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'))
+            testdatas = glob(os.path.join(subdir, '*.png'))
+            count_testfile = 0
+            answer = os.path.basename(subdir)
+            for testdata in testdatas:
+                print('  {} {:.2%} {}'.format(
+                    answer, (count_testfile/len(testdatas)), testdata), datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'))
+                label = classify(testdata)
+
+                count_testfile += 1
+                count_items_all += 1
+                if label == answer:
+                    count_items_correct += 1
+
+    if count_items_all > 0:
+        mes = 'Complete. accuracy:{} / {} = {:.2%}'.format(
+            count_items_correct, count_items_all, count_items_correct / count_items_all)
+    else:
+        mes = 'Complete.'
+    mes += ' ' + datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    print(mes)
+    with open(os.path.join(scrpath, result_filename), mode='a') as f:
+        f.write(mes + '\n')
+
 
 
 if __name__ == '__main__':
