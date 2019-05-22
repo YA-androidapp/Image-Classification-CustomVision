c46910592e8a79bd896567f3dec5655aff41d26b
YA <YA-androidapp@users.noreply.github.com>
2019-05-22 17:57:31 +0900

    Update test.py

--- test.py
+++ test.py
@@ -11,23 +11,27 @@ inifile.read('./.key', 'UTF-8')
 url = inifile.get('customvision', 'url')
 predictionkey = inifile.get('customvision', 'predictionkey')
 
-# url="https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/image?iterationId=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-headers={'content-type':'application/octet-stream','Prediction-Key':predictionkey}
+# url='https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/image?iterationId=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
+headers = {'content-type': 'application/octet-stream',
+           'Prediction-Key': predictionkey}
 
 
 def classify(path):
     try:
         with open(path, 'rb') as images_file:
-            response =requests.post(url,data=open("image01.jpg","rb"),headers=headers)
+            response = requests.post(
+                url, data=open(path, 'rb'), headers=headers)
             response.raise_for_status()
             analysis = response.json()
-            name, pred = analysis["predictions"][0]["tagName"], analysis["predictions"][0]["probability"]
+            name, pred = analysis['predictions'][0]['tagName'], analysis['predictions'][0]['probability']
             print(name, pred)
     except Exception as e:
-        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+        print('[Err] {0}'.format(e))
+
 
 def main():
-    pass
+    classify('test.png')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
